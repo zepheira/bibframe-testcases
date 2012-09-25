db9a64331a46cbb93a424ea7556935ad47d0ab91
Eric Miller <em@zepheira.com>
2012-09-25 09:16:50 -0400

    updating div to support fragment ID

--- cmdline/build_model.py
+++ cmdline/build_model.py
@@ -91,7 +91,7 @@ T0 = '''<!DOCTYPE html>
   
       </div>
 
-      <div class="specific-types-display">
+      <div class="specific-types-display types">
 
         <div class="row-fluid">
          <div class="span12">
