170c2d4425c1fbe103767a07921e9cbf13e078cc
Uche Ogbuji <uche@ogbuji.net>
2013-04-23 12:03:58 -0600

    Prune to just the test cases, part 1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name = 'btframework',
-    version = '0.2',
-    description = 'BTFramework',
+    name = 'bibframe-testcases',
+    version = '0.3',
+    description = 'BIBFRAME Test Cases',
     license = 'License :: OSI Approved :: Apache Software License',
     author = 'Uche Ogbuji',
     author_email = 'uche@zepheira.com',
     url = 'http://zepheira.com/',
-    package_dir={'btframework': 'lib'},
-    #packages = ['btframework', 'btframework.akara'],
-    packages = ['btframework', 'btframework.contrib'],
-    scripts = ['cmdline/readmarcxml.py', 'cmdline/augment_lccn.py', 'cmdline/build_model.py', 'cmdline/build_testcase.py'],
+    #package_dir={'bibframe.testcases': 'lib'},
+    #packages = ['bibframe', 'bibframe.contrib'],
+    scripts = ['builder/build_testcase.py'],
     #http://packages.python.org/distribute/setuptools.html#declaring-dependencies
 #    install_requires = ['amara >= 2', 'uritemplate >= 0.5.1'],
 )
+
